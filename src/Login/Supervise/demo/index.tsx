a812f188c5d12b1643ac8af996ff8060ad99c916
huangqian <huangqian@vortexinfo.cn>
2020-09-22 16:59:50 +0800

    添加prettier依赖

--- src/Login/Supervise/demo/index.tsx
+++ src/Login/Supervise/demo/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { Login } from 'wind'
-const { Supervise } = Login
+import { Login } from 'wind';
+const { Supervise } = Login;
 
 const SuperviseDemo = () => {
   return (
-    <Supervise onFinish={() => {
-      console.log(123)
-    }} />
-  )
-}
+    <Supervise
+      onFinish={() => {
+        console.log(123);
+      }}
+    />
+  );
+};
 
-export default SuperviseDemo
+export default SuperviseDemo;
