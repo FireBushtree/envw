0a744cba83deb3540b19326eddc7e59bd9135020
huangqian <huangqian@vortexinfo.cn>
2020-09-22 16:52:47 +0800

    去除打包后存在demo文件夹

---
+++ script/remove-demo.js
@@ -0,0 +1,13 @@
+const rimraf = require('rimraf')
+
+const remove = (filePath, name) => {
+  rimraf(filePath, {}, (error) => {
+    if (error) {
+      console.log(error)
+    }
+    console.log(`remove ${name} demo folder success`)
+  })
+}
+
+remove('lib/**/demo', 'lib')
+remove('es/**/demo', 'es')
